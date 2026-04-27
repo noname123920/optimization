c9ec0006f3f68f857e7c3c8a97eaab77bf3cdaa9
Kitkuk <110992990+noname123920@users.noreply.github.com>
2026-04-27 18:53:35 +0500

    /

---
+++ сглаживание/main2.m
@@ -0,0 +1,21 @@
+clc;
+
+a = 0; b = 1; h = 0.05; alpha = 1;
+
+C = create_matrix(alpha, h, a, b);
+
+n = round((b - a) / h);
+i = 0:n;
+s = a + i * h;
+
+z_ex = sin(i * h / n)';
+b_vec = C * z_ex;
+z_new = C \ b_vec;
+err = max(abs(z_ex - z_new));
+
+figure;
+plot(s, z_new);
+grid on;
+xlabel('s');
+ylabel('z');
+
